f842de2fa0918db58727f5811ea37b131114aa23
Okwor Chukwuemeka Nnajiofor <thenaijadev@gmail.com>
2023-08-26 09:58:25 +0100

    Done

--- lib/features/cart/presentation/screens/cart_screen.dart
+++ lib/features/cart/presentation/screens/cart_screen.dart
@@ -16,24 +16,33 @@ class CartScreen extends StatelessWidget {
         ),
         elevation: 0,
         backgroundColor: const Color.fromARGB(0, 255, 255, 255),
-        leading: Transform.translate(
-          offset: const Offset(20, 0),
-          child: Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: Material(
-              color: Colors.white,
-              elevation: 2,
-              borderRadius: BorderRadius.circular(10),
-              child: const Icon(
-                Icons.chevron_left,
-                color: Colors.black,
-                size: 20,
+        leading: GestureDetector(
+          onTap: () {
+            Navigator.pop(context);
+          },
+          child: Transform.translate(
+            offset: const Offset(20, 0),
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Material(
+                color: Colors.white,
+                elevation: 2,
+                borderRadius: BorderRadius.circular(10),
+                child: const Icon(
+                  Icons.chevron_left,
+                  color: Colors.black,
+                  size: 20,
+                ),
               ),
             ),
           ),
         ),
       ),
-      body: ListView(children: const [CartItemWidget()]),
+      body: ListView(children: [
+        CartItemWidget(
+          onDelete: () {},
+        )
+      ]),
     );
   }
 }
--- lib/features/cart/presentation/widgets/cart_item_widget.dart
+++ lib/features/cart/presentation/widgets/cart_item_widget.dart
@@ -3,91 +3,132 @@ import 'package:the_food_hub_nsk_nig/core/constants/app_colors.dart';
 import 'package:the_food_hub_nsk_nig/core/widgets/text_widget.dart';
 import 'package:the_food_hub_nsk_nig/features/cart/presentation/widgets/quantity_button.dart';
 
-class CartItemWidget extends StatelessWidget {
-  const CartItemWidget({super.key});
+class CartItemWidget extends StatefulWidget {
+  const CartItemWidget({
+    super.key,
+    required this.onDelete,
+  });
+  final VoidCallback onDelete;
 
+  @override
+  State<CartItemWidget> createState() => _CartItemWidgetState();
+}
+
+class _CartItemWidgetState extends State<CartItemWidget> {
+  bool showDetails = false;
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(20.0),
-      child: Card(
-        shadowColor: AppColors.orange,
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-        color: Colors.white,
-        child: Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-                height: 110,
-                width: 110,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  image: const DecorationImage(
-                      image: AssetImage("assets/images/swallow.jpg"),
-                      fit: BoxFit.cover),
-                ),
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const SizedBox(
-                    width: 210,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        TextWidget(
-                          text: "Rice Meal",
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
-                        ),
-                        Icon(
-                          Icons.delete,
-                          color: AppColors.orange,
-                          size: 18,
-                        )
-                      ],
-                    ),
+      child: GestureDetector(
+        onTap: () {
+          setState(() {
+            showDetails = true;
+          });
+        },
+        child: Card(
+          shadowColor: AppColors.orange,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+          color: Colors.white,
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  height: 110,
+                  width: 110,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    image: const DecorationImage(
+                        image: AssetImage("assets/images/swallow.jpg"),
+                        fit: BoxFit.cover),
                   ),
-                  Container(
-                      padding: const EdgeInsets.only(left: 25),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      width: 210,
                       child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
                         children: [
-                          const TextWidget(
-                            text: "\$15.30",
-                            color: AppColors.orange,
-                            fontWeight: FontWeight.bold,
+                          const Padding(
+                            padding: EdgeInsets.only(left: 12.0),
+                            child: TextWidget(
+                              text: "Rice Meal",
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
-                          QuantityButton(
-                            onTap: () {},
-                            isMinus: true,
+                          const SizedBox(
+                            width: 20,
                           ),
-                          QuantityButton(
-                            onTap: () {},
-                            isMinus: false,
+                          GestureDetector(
+                            onTap: widget.onDelete,
+                            child: const Icon(
+                              Icons.delete,
+                              color: AppColors.orange,
+                              size: 18,
+                            ),
                           )
                         ],
-                      )),
-                  const Padding(
-                    padding: EdgeInsets.only(left: 27.0),
-                    child: Row(
-                      children: [
-                        SizedBox(
-                            width: 100,
-                            child: TextWidget(
-                              overflow: TextOverflow.ellipsis,
-                              fontSize: 14,
-                              text: "Coconut rice, Fried Chiken, Coke,",
-                              color: AppColors.detailsGrey,
-                            )),
-                        Icon(Icons.keyboard_arrow_down_rounded)
-                      ],
+                      ),
                     ),
-                  )
-                ],
-              )
-            ],
+                    Container(
+                        padding: const EdgeInsets.only(
+                            left: 25, top: 15, bottom: 15),
+                        child: Row(
+                          children: [
+                            const TextWidget(
+                              text: "\$15.30",
+                              color: AppColors.orange,
+                              fontWeight: FontWeight.bold,
+                            ),
+                            const SizedBox(
+                              width: 20,
+                            ),
+                            QuantityButton(
+                              onTap: () {},
+                              isMinus: true,
+                            ),
+                            const SizedBox(
+                              width: 12,
+                            ),
+                            const TextWidget(
+                              text: "02",
+                              fontWeight: FontWeight.bold,
+                            ),
+                            const SizedBox(
+                              width: 12,
+                            ),
+                            QuantityButton(
+                              onTap: () {},
+                              isMinus: false,
+                            )
+                          ],
+                        )),
+                    const Padding(
+                      padding: EdgeInsets.only(left: 27.0),
+                      child: Row(
+                        children: [
+                          SizedBox(
+                              width: 100,
+                              child: TextWidget(
+                                overflow: TextOverflow.ellipsis,
+                                fontSize: 14,
+                                text: "Coconut rice, Fried Chiken, Coke,",
+                                color: AppColors.detailsGrey,
+                              )),
+                          Icon(Icons.keyboard_arrow_down_rounded)
+                        ],
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
           ),
         ),
       ),
